0135bf5c0d8562dea6a75688d0afe62d81c0647c
gness1804 <graham.nessler@gmail.com>
2022-05-17 15:59:30 -0500

    subset tests passing

--- custom-set.ts
+++ custom-set.ts
@@ -1,7 +1,7 @@
 export class CustomSet {
   initial: number[];
   constructor(initial: number[] = []) {
-   this.initial = initial;
+    this.initial = initial;
   }
 
   empty(): boolean {
@@ -16,8 +16,15 @@ export class CustomSet {
     throw new Error('Remove this statement and implement this function')
   }
 
-  subset(other: unknown): CustomSet {
-    throw new Error('Remove this statement and implement this function')
+  subset(otherSet: CustomSet): boolean {
+    // if the other set has all elements of the original set
+    const failedCount = this.initial.reduce((total: number, thisSetNumber: number) => {
+      if (otherSet.initial.indexOf(thisSetNumber) === -1) {
+        total++;
+      }
+      return total;
+    }, 0)
+    return failedCount === 0;
   }
 
   disjoint(other: unknown): CustomSet {
